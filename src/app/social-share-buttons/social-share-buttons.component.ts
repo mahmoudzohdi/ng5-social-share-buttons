5a1fe2a4b37dc9f24753988cfd3063a4c783d20c
Mahmoud Zohdi <mahmoud.zohdi@gmail.com>
2019-06-17 21:07:44 +0300

    handle close tooltip if clicked outside SocialShareButtonsComponent

--- src/app/social-share-buttons/social-share-buttons.component.ts
+++ src/app/social-share-buttons/social-share-buttons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'social-share-buttons',
@@ -9,9 +9,17 @@ export class SocialShareButtonsComponent implements OnInit {
 
   showTooltip: boolean = false;
 
-  constructor() { }
+  constructor(
+    private renderer: Renderer2,
+    private _elementRef: ElementRef
+  ) { }
 
   ngOnInit() {
+    this.renderer.listen('window', 'click',(e:Event)=>{
+      if(e.target !== this._elementRef.nativeElement && !this._elementRef.nativeElement.contains(e.target)){
+          this.showTooltip = false;
+      }
+    });
   }
 
   toggleSocialShareTooltip(){
